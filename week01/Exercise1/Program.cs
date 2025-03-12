846e76a6d6c0352c2901a4cb110376923875c76f
Jeremiah Egwa <jerriee232@gmail.com>
2025-03-12 02:06:02 +0100

    finished week01 Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask for user name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Print out user name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
